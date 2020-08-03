b4fed80573ccfaf3d41f516066050d73c871169c
navarichN <54509830+navarichN@users.noreply.github.com>
2020-08-03 21:33:29 +0300

    add interactivity

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import React from 'react';
 import './App.css';
-import {Card} from './components/Card/Card'
+import {Card} from './components/Card/Card';
 
 class App extends React.Component {
   constructor(props) {
       super(props)
       this.state = {
         pokemons: [
-          {name: 'bulbasaur', src: './images/green.png'},
-          {name: 'pikachu', src: './images/pika.png'},
-          {name: 'swarm', src: './images/bee.png'},
-          {name: 'bulbasaur', src: './images/bee.png'},
-          {name: 'pikachu', src: './images/green.png'},
-          {name: 'swarm', src: './images/green.png'},
-          {name: 'bulbasaur', src: './images/dragon.png'},
-          {name: 'pikachu', src: './images/green.png'},
-          {name: 'swarm', src: './images/green.png'},
-          {name: 'bulbasaur', src: './images/bee.png'},
-          {name: 'pikachu', src: './images/pika.png'},
-          {name: 'pikachu', src: './images/pika.png'},
-          {name: 'pikachu', src: './images/pika.png'},
-          {name: 'pikachu', src: './images/pika.png'},
-          {name: 'swarm', src: './images/bee.png'},
-          {name: 'swarm', src: './images/bee.png'},
+          {name: 'bulbasaur', src: './images/green.png', id: 'a'},
+          {name: 'pikachu', src: './images/pika.png', id: 'b'},
+          {name: 'swarm', src: './images/bee.png', id: 'c'},
+          {name: 'bulbasaur', src: './images/bee.png', id: 'd'},
+          {name: 'pikachu', src: './images/green.png', id: 'e'},
+          {name: 'swarm', src: './images/green.png', id: 'f'},
+          {name: 'bulbasaur', src: './images/dragon.png', id: 'g'},
+          {name: 'pikachu', src: './images/green.png', id: 'aa'},
+          {name: 'swarm', src: './images/green.png', id: 'bb'},
+          {name: 'bulbasaur', src: './images/bee.png', id: 'cc'},
+          {name: 'pikachu', src: './images/pika.png', id: 'dd'},
+          {name: 'pikachu', src: './images/pika.png', id: 'ee'},
+          {name: 'pikachu', src: './images/pika.png', id: 'ff'},
+          {name: 'pikachu', src: './images/pika.png', id: 'aaa'},
+          {name: 'swarm', src: './images/bee.png', id: 'bbb'},
+          {name: 'swarm', src: './images/bee.png', id: 'ccc'},
         ],
-        title: "Pokemons"
+        title: "Pokemons",
+        counter: 0
       }
+      this.counterFunc = this.counterFunc.bind(this);
+  }
+  counterFunc(event){
+    if(event.target.innerText === "ПОЙМАТЬ"){
+      this.setState((state) => {
+        return {counter: state.counter + 1};
+      })
+    } else if(event.target.innerText === "ОТПУСТИТЬ"){
+      this.setState((state) => {
+        return {counter: state.counter - 1};
+      })
+    }
+    return this.state.counter
   }
 
   render(){
@@ -34,19 +48,20 @@ class App extends React.Component {
     return (
       <div className="app">
         <h1>Поймано покемонов</h1>
-        <div className="cards">
+        <span className = "counter">{this.state.counter}/{pokemons.length}</span>
+        <div className="cards" onClick={this.counterFunc}>
           {pokemons.map((el, index) => {
             return(
-              <Card name = {el.name} src = {el.src} key = {index}/>
+              <div>
+                <Card name = {el.name} src = {el.src} key = {el.name + index}/> 
+                {/* почему-то не работает key */}
+              </div>
             )
           })}
         </div>
-        
       </div>
     )
   }
-
-
 }
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,37 @@
-/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './Card.css';
 
-export const Card = (props) => {
-    return(
-        <div className="card">
-            <h3 className="card__title">{props.name}</h3>
-            <img src = {props.src} width = "96px" height = "96px" className="card__img"/>
-            <button className="card__btn">ПОЙМАТЬ</button>
+export class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          isToggleOn: true,
+          src: props.src,
+          name: props.name,
+          counter: 0
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(event) {
+      this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+      if(this.state.isToggleOn){
+        event.currentTarget.className = 'card card_red'
+      } else {
+        event.currentTarget.className = 'card'
+      }
+    }
+
+    render() {
+      return (
+        <div className="card" onClick = {this.handleClick}>
+            <h3 className="card__title">{this.state.name}</h3>
+            <img src = {this.state.src} width = "96px" height = "96px" className="card__img" alt = "pokemon"/>
+            <button className = "card__btn">
+                {this.state.isToggleOn ? 'ПОЙМАТЬ' : 'ОТПУСТИТЬ'}
+            </button>
         </div>
-    )
-};
+      );
+    }
+}
